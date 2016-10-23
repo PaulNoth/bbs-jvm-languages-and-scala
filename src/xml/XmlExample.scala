feba7c68e3d11d0874ae9250271a508959352bca
Paul P <paulp@hotmail.sk>
2016-10-23 19:27:07 +0200

    fixed text in XML example

--- src/xml/XmlExample.scala
+++ src/xml/XmlExample.scala
@@ -4,7 +4,7 @@ object XmlExample extends App {
 
   val xml = <a><b atribute="att">inside text</b></a>
 
-  val myText = "this is my brownbag ession"
+  val myText = "this is my brownbag session"
   val evaluatedLiteral = <a><b atribute="att">{myText}</b></a>
 
   println(evaluatedLiteral)
